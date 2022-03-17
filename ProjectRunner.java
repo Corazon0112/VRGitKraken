7f7803626e515e127d17c2f26edf60d9e8b00ea2
Manan Shah <mananhshah04@gmail.com>
2022-03-16 21:52:04 -0400

    Print quotient runner and interface created

--- ProjectRunner.java
+++ ProjectRunner.java
@@ -1,3 +1,4 @@
+<<<<<<< main
 public class ProjectRunner {
 	public static void main(String [] args)
     {
@@ -11,4 +12,15 @@ public class ProjectRunner {
 	interface perfectSquare{
 		boolean square(int x);
 	}
+=======
+public class ProjectRunner{ 
+    public static void main(String [] args){
+        Quotient d= (a,b) -> System.out.printf("Number is approximately %.5f", a/b);
+        d.divide(5.2988f,5.0032f);
+        System.out.println();
+    }
+    interface Quotient{
+        void divide(float a, float b);
+    }         
+>>>>>>> Print quotient runner and interface created
 }
