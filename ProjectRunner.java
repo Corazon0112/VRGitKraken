9c2220c9c7b36cfd298cad6d7590566563804616
Tushar <tusharcs0112@gmail.com>
2022-03-16 22:09:38 -0400

    Combined everything into one file

--- ProjectRunner.java
+++ ProjectRunner.java
@@ -1,5 +1,5 @@
-<<<<<<< main
 // Tushar - I learned how to use Github to create repositories and work on code with another person at the same time. This will help me in the future to create repositories, whether or not I am working with someone else.
+//Manan - From this I learned how to create repositories using Github and how to collabrate with another person/team in order to organize changes made to files.
 public class ProjectRunner {
 	public static void main(String [] args)
     {
@@ -9,20 +9,14 @@ public class ProjectRunner {
 	    	return sqrt-(int)sqrt==0;
 	    };
 	    System.out.println(perfectsquare.square(100));
-    }
-	interface perfectSquare{
-		boolean square(int x);
-	}
-=======
-//Manan - From this I learned how to create repositories using Github and how to collabrate with another person/team in order to organize changes made to files.
-public class ProjectRunner{ 
-    public static void main(String [] args){
         Quotient d= (a,b) -> System.out.printf("Number is approximately %.5f", a/b);
         d.divide(5.2988f,5.0032f);
         System.out.println();
     }
+	interface perfectSquare{
+		boolean square(int x);
+	}
     interface Quotient{
         void divide(float a, float b);
-    }         
->>>>>>> Print quotient runner and interface created
+    } 
 }
